07acdc9567bc6c573f123a63d7ae33e4ffbcac13
unknown <bogdan.lyamzin.d@gmail.com>
2023-10-10 16:37:22 +0300

    finish lesson 3

---
+++ lesson-3/express-example-1/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express(); // app - web-server
+
+app.get("/", (request, response) => {
+    response.send("<h1>Home page</h1>")
+})
+
+app.get("/contacts", (request, response)=> {
+    console.log(request.url);
+    console.log(request.method);
+    response.send("<h1>Contacts page</h1>")
+})
+
+app.listen(3000, (error)=> {
+    if(error){
+        return console.log(`Server run failed. ${error.message}`)
+    }
+    console.log("Server running on 3000 PORT")
+})
