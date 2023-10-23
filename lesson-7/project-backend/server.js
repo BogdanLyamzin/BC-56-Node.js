2b518020d389495634db38e1efe2807e7e03d03d
unknown <bogdan.lyamzin.d@gmail.com>
2023-10-23 18:27:41 +0300

    finish lesson 7

---
+++ lesson-7/project-backend/server.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+import app from "./app.js";
+
+mongoose.connect(process.env.DB_HOST)
+  .then(()=> {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
